682fc8fc3be46900b44960b20a36fd7f958ccbb2
gaz-oracle <fco.gaz.crespo@gmail.com>
2023-04-04 18:33:51 -0600

    2 º Commit Initial

--- src/main/java/com/gaz/app/dicovsse/DiCovsSeApplication.java
+++ src/main/java/com/gaz/app/dicovsse/DiCovsSeApplication.java
@@ -2,21 +2,29 @@ package com.gaz.app.dicovsse;
 
 import com.gaz.app.dicovsse.controller.VideoController;
 import com.gaz.app.dicovsse.repository.VideoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
-public class DiCovsSeApplication {
+public class DiCovsSeApplication
+{
 
-    public static void main(String[] args)  {
-        VideoRepository repository = new VideoRepository();
-        VideoController controller = new VideoController(repository);
-        String response = controller.next();
-        System.out.println(response);
-
-
-        SpringApplication.run(DiCovsSeApplication.class, args);
+    public static void main(String[] args)
+    {
+        ConfigurableApplicationContext app = SpringApplication.run(DiCovsSeApplication.class, args);
+       Arrays.stream(app.getBeanDefinitionNames()).forEach(System.out::println);
     }
-
-
+       @Bean
+        public CommandLineRunner commandLineRunner ()
+       {
+           return args ->{
+               System.out.println("Hello from the commandLineRunner");
+           };
+        }
 }
